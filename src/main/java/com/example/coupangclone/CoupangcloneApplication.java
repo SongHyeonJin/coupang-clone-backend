eb7da3a8a8e7c2856c2baddadba461046add9df8
HyeonJin Song <shwj1602@naver.com>
2025-03-24 11:28:34 +0900

    init : 프로젝트 구조 및 기본 설정

---
+++ src/main/java/com/example/coupangclone/CoupangcloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.coupangclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupangcloneApplication.class, args);
+	}
+
+}
