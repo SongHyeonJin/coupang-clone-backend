eecafcaa2403b6410b1bd9c352cae06ef81bdc41
HyeonJin Song <shwj1602@naver.com>
2025-03-24 19:26:37 +0900

    feat(user): 로그인 기능 구현 (Controller & Service)

--- src/main/java/com/example/coupangclone/user/controller/UserController.java
+++ src/main/java/com/example/coupangclone/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.coupangclone.user.controller;
 
+import com.example.coupangclone.user.dto.LoginRequestDto;
 import com.example.coupangclone.user.dto.SignupRequestDto;
 import com.example.coupangclone.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,12 @@ public class UserController {
     @PostMapping("/api/signup")
     public ResponseEntity<?> siggup(@Valid @RequestBody SignupRequestDto requestDto) {
         return userService.signup(requestDto);
+    }
+
+    @PostMapping("/api/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto requestDto,
+                                    HttpServletResponse response) {
+        return userService.login(requestDto, response);
     }
 
 }
