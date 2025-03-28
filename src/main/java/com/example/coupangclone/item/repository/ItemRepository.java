2d50d4e3bda0cb6b0bb37c8a18bf4f55c8cddab7
HyeonJin Song <shwj1602@naver.com>
2025-03-28 20:51:02 +0900

    feat(item): 아이템 이름 및 브랜드로 검색하는 쿼리 추가

--- src/main/java/com/example/coupangclone/item/repository/ItemRepository.java
+++ src/main/java/com/example/coupangclone/item/repository/ItemRepository.java
@@ -1,5 +1,6 @@
 package com.example.coupangclone.item.repository;
 
+import com.example.coupangclone.item.entity.Brand;
 import com.example.coupangclone.item.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,10 +8,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query("SELECT i FROM Item i LEFT JOIN i.brand b " +
             "WHERE i.name LIKE %:keyword% OR (b IS NOT NULL AND b.name LIKE %:keyword%)")
     Page<Item> searchByNameOrBrand(@Param("keyword") String keyword, Pageable pageable);
+
+    @Query("SELECT i FROM Item i LEFT JOIN i.brand b " +
+            "WHERE LOWER(i.name) LIKE LOWER(%:keyword%) " +
+            "OR (b IS NOT NULL AND LOWER(b.name) LIKE LOWER(%:keyword%))")
+    List<Item> findMatchingItemsByNameOrBrand(@Param("keyword") String keyword);
+
+    List<Item> findTop5ByBrand(Brand brand);
 
 }
