b860f9fcb60d97971ffd71951f4dac51767bb116
HyeonJin Song <shwj1602@naver.com>
2025-03-31 18:41:21 +0900

    test(user): 로그인 서비스 로직 테스트 코드 추가

--- src/test/java/com/example/coupangclone/user/service/UserServiceTest.java
+++ src/test/java/com/example/coupangclone/user/service/UserServiceTest.java
@@ -1,15 +1,20 @@
 package com.example.coupangclone.user.service;
 
+import com.example.coupangclone.auth.jwt.JwtProvider;
 import com.example.coupangclone.global.dto.ErrorResponseDto;
+import com.example.coupangclone.global.exception.ErrorException;
 import com.example.coupangclone.global.exception.ExceptionEnum;
+import com.example.coupangclone.user.dto.LoginRequestDto;
 import com.example.coupangclone.user.dto.SignupRequestDto;
 import com.example.coupangclone.user.dto.UserResponseDto;
 import com.example.coupangclone.user.entity.User;
 import com.example.coupangclone.user.enums.UserRoleEnum;
 import com.example.coupangclone.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -30,6 +36,9 @@ class UserServiceTest {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
 
     @AfterEach
     void tearDown() {
@@ -48,8 +57,8 @@ class UserServiceTest {
 
         // then
         assertThat(response.getStatusCode().value()).isEqualTo(200);
-
         assertThat(response.getBody()).isInstanceOf(UserResponseDto.class);
+
         UserResponseDto userResponseDto = (UserResponseDto) response.getBody();
         assertThat(userResponseDto.getName()).isEqualTo(request.getName());
         assertThat(userResponseDto.getMsg()).isEqualTo("회원가입 성공");
@@ -63,14 +72,7 @@ class UserServiceTest {
         SignupRequestDto request =
                 createUserDto(email, "qwer123!", "홍길동", "01012345678", "남성");
         String password = "qwer123!";
-        User user = User.builder()
-                .email(email)
-                .password(passwordEncoder.encode(password))
-                .name("김서방")
-                .tel("01032145643")
-                .gender("남성")
-                .role(UserRoleEnum.USER)
-                .build();
+        User user = createUser(email, password, "김서방", "01043215678", "남성");
         userRepository.save(user);
 
         // when
@@ -78,14 +80,74 @@ class UserServiceTest {
 
         // then
         assertThat(response.getStatusCode().value()).isEqualTo(400);
-
         assertThat(response.getBody()).isInstanceOf(ErrorResponseDto.class);
+
         ErrorResponseDto errorResponseDto = (ErrorResponseDto) response.getBody();
         assertThat(errorResponseDto.getType()).isEqualTo("USER");
         assertThat(errorResponseDto.getMsg()).isEqualTo(ExceptionEnum.EMAIL_DUPLICATION.getMsg());
     }
 
-    private static SignupRequestDto createUserDto(String email, String password, String name, String tel, String gender) {
+    @DisplayName("회원인 사람이 로그인을 성공한다.")
+    @Test
+    void login_success(){
+        // given
+        LoginRequestDto requestDto = createLoginDto("test@example.com", "qwer123!");
+        String encodedPwd = passwordEncoder.encode(requestDto.getPassword());
+        User user = createUser(requestDto.getEmail(), encodedPwd, "홍길동", "01012345678", "남성");
+        userRepository.save(user);
+
+
+        // when
+        HttpServletResponse httpServletResponse = Mockito.mock(HttpServletResponse.class);
+        String token = jwtProvider.createToken(user.getId(), user.getEmail(), user.getName(), user.getRole());
+        httpServletResponse.addHeader(JwtProvider.AUTHORIZATION_HEADER, token);
+
+        ResponseEntity<?> response = userService.login(requestDto, httpServletResponse);
+
+        // then
+        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        assertThat(response.getBody()).isInstanceOf(UserResponseDto.class);
+
+        UserResponseDto userResponseDto = (UserResponseDto) response.getBody();
+        assertThat(userResponseDto.getName()).isEqualTo(user.getName());
+        assertThat(userResponseDto.getMsg()).isEqualTo("로그인 성공");
+    }
+
+    @DisplayName("이메일을 잘못 입력할 시 로그인을 실패한다.")
+    @Test
+    void login_fail_wrongEmail(){
+        // given
+        LoginRequestDto requestDto = createLoginDto("test@example.com", "qwer123!");
+        String encodedPwd = passwordEncoder.encode(requestDto.getPassword());
+        User user = createUser(requestDto.getEmail(), encodedPwd, "홍길동", "01012345678", "남성");
+        userRepository.save(user);
+
+        requestDto.updateEmail("test1@example.com");
+
+        // when // then
+        assertThatThrownBy(() -> userService.login(requestDto, null))
+                .isInstanceOf(ErrorException.class)
+                .hasMessage(ExceptionEnum.USER_NOT_FOUND.getMsg());
+    }
+
+    @DisplayName("비밀번호를 잘못 입력 시 로그인을 실패한다.")
+    @Test
+    void login_fail_wrongPassword(){
+        // given
+        LoginRequestDto requestDto = createLoginDto("test@example.com", "qwer123!");
+        String encodedPwd = passwordEncoder.encode(requestDto.getPassword());
+        User user = createUser(requestDto.getEmail(), encodedPwd, "홍길동", "01012345678", "남성");
+        userRepository.save(user);
+
+        requestDto.updatePassword("asdf123!");
+
+        // when // then
+        assertThatThrownBy(() -> userService.login(requestDto, null))
+                .isInstanceOf(ErrorException.class)
+                .hasMessage(ExceptionEnum.WRONG_PASSWORD.getMsg());
+    }
+
+    private SignupRequestDto createUserDto(String email, String password, String name, String tel, String gender) {
         return SignupRequestDto.builder()
                 .email(email)
                 .password(password)
@@ -95,14 +157,21 @@ class UserServiceTest {
                 .build();
     }
 
-    private static User createUser(SignupRequestDto requestDto, String password, UserRoleEnum role) {
-        return User.builder()
-                .email(requestDto.getEmail())
+    private LoginRequestDto createLoginDto(String email, String password) {
+        return LoginRequestDto.builder()
+                .email(email)
                 .password(password)
-                .name(requestDto.getName())
-                .tel(requestDto.getTel())
-                .gender(requestDto.getGender())
-                .role(role)
+                .build();
+    }
+
+    private User createUser(String email, String password, String name, String tel, String gender) {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .tel(tel)
+                .gender(gender)
+                .role(UserRoleEnum.USER)
                 .build();
     }
 
