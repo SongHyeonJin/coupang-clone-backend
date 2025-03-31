ecd7c2b6144940bb64a03ccdec2a6081d25a2384
HyeonJin Song <shwj1602@naver.com>
2025-03-31 18:20:34 +0900

    refactor(user): LoginRequestDto 유효성 검사 @NotBlank로 개선

--- src/main/java/com/example/coupangclone/user/dto/LoginRequestDto.java
+++ src/main/java/com/example/coupangclone/user/dto/LoginRequestDto.java
@@ -1,15 +1,15 @@
 package com.example.coupangclone.user.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class LoginRequestDto {
-    @NotNull(message = "E-mail을 입력해주세요.")
+    @NotBlank(message = "E-mail을 입력해주세요.")
     private String email;
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
 }
