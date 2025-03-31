fd828e721a14ddf6ffcfd55e3b091d4607a01160
HyeonJin Song <shwj1602@naver.com>
2025-03-31 18:40:30 +0900

    feat(user): LoginRequestDto에 @Builder 및 테스트를 위한 필드 수정 메서드 추가

--- src/main/java/com/example/coupangclone/user/dto/LoginRequestDto.java
+++ src/main/java/com/example/coupangclone/user/dto/LoginRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.coupangclone.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,5 +12,19 @@ public class LoginRequestDto {
     private String email;
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+
+    @Builder
+    public LoginRequestDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public void updateEmail(String email) {
+        this.email = email;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 
 }
