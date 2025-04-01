a229e32b0817117be32ae8427e65b8f19eafbfae
HyeonJin Song <shwj1602@naver.com>
2025-04-01 16:03:21 +0900

    test(item): AdminItemService 브랜드 등록 기능 테스트 추가

--- src/test/java/com/example/coupangclone/item/service/AdminItemServiceTest.java
+++ src/test/java/com/example/coupangclone/item/service/AdminItemServiceTest.java
@@ -3,9 +3,12 @@ package com.example.coupangclone.item.service;
 import com.example.coupangclone.global.dto.BasicResponseDto;
 import com.example.coupangclone.global.exception.ErrorException;
 import com.example.coupangclone.global.exception.ExceptionEnum;
+import com.example.coupangclone.item.dto.brand.BrandRequestDto;
 import com.example.coupangclone.item.dto.category.CategoryRequestDto;
+import com.example.coupangclone.item.entity.Brand;
 import com.example.coupangclone.item.entity.Category;
 import com.example.coupangclone.item.enums.ItemTypeEnum;
+import com.example.coupangclone.item.repository.BrandRepository;
 import com.example.coupangclone.item.repository.CategoryRepository;
 import com.example.coupangclone.user.entity.User;
 import com.example.coupangclone.user.enums.UserRoleEnum;
@@ -31,16 +34,19 @@ class AdminItemServiceTest {
     private UserRepository userRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private BrandRepository brandRepository;
 
     @AfterEach
     void tearDown() {
+        brandRepository.deleteAllInBatch();
         categoryRepository.deleteAllInBatch();
         userRepository.deleteAllInBatch();
     }
 
     @DisplayName("관리자가 카테고리를 등록하는데 성공한다.")
     @Test
-    void createCategory_success(){
+    void createCategory_success() {
         // given
         User admin =
                 createUser("admin@example.com", "qwer123!", "관리자", "01098765432", "남성");
@@ -60,7 +66,7 @@ class AdminItemServiceTest {
 
     @DisplayName("관리자가 아닌 회원이 카테고리를 등록하려하면 실패한다.")
     @Test
-    void createCategory_fail_not_admin(){
+    void createCategory_fail_not_admin() {
         // given
         User user = User.builder()
                 .email("user@example.com")
@@ -81,7 +87,7 @@ class AdminItemServiceTest {
 
     @DisplayName("존재하는 카테고리 등록 시 실패한다.")
     @Test
-    void createCategory_fail_existsCategory(){
+    void createCategory_fail_existsCategory() {
         // given
         User admin =
                 createUser("admin@example.com", "qwer123!", "관리자", "01098765432", "남성");
@@ -98,7 +104,7 @@ class AdminItemServiceTest {
 
     @DisplayName("카테고리의 상위 카테고리 등록 시 상위 카테고리가 존재하지 않으면 실패한다.")
     @Test
-    void createCategory_fail_not_found_parentCategory(){
+    void createCategory_fail_not_found_parentCategory() {
         // given
         User admin =
                 createUser("admin@example.com", "qwer123!", "관리자", "01098765432", "남성");
@@ -115,6 +121,72 @@ class AdminItemServiceTest {
         assertThatThrownBy(() -> adminItemService.createCategory(requestDto, admin))
                 .isInstanceOf(ErrorException.class)
                 .hasMessage(ExceptionEnum.CATEGORY_NOT_FOUND.getMsg());
+    }
+
+    @DisplayName("관리자가 브랜드를 등록하는데 성공한다.")
+    @Test
+    void createBrand_success() {
+        // given
+        User admin =
+                createUser("admin@example.com", "qwer123!", "관리자", "01098765432", "남성");
+        userRepository.save(admin);
+        BrandRequestDto requestDto = BrandRequestDto.builder()
+                .name("애플")
+                .build();
+
+        // when
+        ResponseEntity<?> response = adminItemService.createBrand(requestDto, admin);
+
+        // then
+        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        assertThat(response.getBody()).isInstanceOf(BasicResponseDto.class);
+
+        BasicResponseDto responseDto = (BasicResponseDto) response.getBody();
+        assertThat(responseDto.getMsg()).isEqualTo("브랜드 등록이 완료되었습니다.");
+    }
+
+    @DisplayName("관리자가 아닌 회원이 브랜드를 등록하려하면 실패한다.")
+    @Test
+    void createBrand_fail_not_admin() {
+        // given
+        User user = User.builder()
+                .email("user@example.com")
+                .password("qwer123!")
+                .name("회원")
+                .tel("01032153215")
+                .gender("여성")
+                .role(UserRoleEnum.USER)
+                .build();
+        userRepository.save(user);
+        BrandRequestDto requestDto = BrandRequestDto.builder()
+                .name("애플")
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> adminItemService.createBrand(requestDto, user))
+                .isInstanceOf(ErrorException.class)
+                .hasMessage(ExceptionEnum.NOT_ALLOW.getMsg());
+    }
+
+    @DisplayName("존재하는 브랜드 등록 시 실패한다.")
+    @Test
+    void createBrand_fail_existsBrand() {
+        // given
+        User admin =
+                createUser("admin@example.com", "qwer123!", "관리자", "01098765432", "남성");
+        userRepository.save(admin);
+        BrandRequestDto requestDto = BrandRequestDto.builder()
+                .name("애플")
+                .build();
+        Brand brand = Brand.builder()
+                .name("애플")
+                .build();
+        brandRepository.save(brand);
+
+        // when // then
+        assertThatThrownBy(() -> adminItemService.createBrand(requestDto, admin))
+                .isInstanceOf(ErrorException.class)
+                .hasMessage(ExceptionEnum.BRAND_DUPLICATION.getMsg());
     }
 
 
